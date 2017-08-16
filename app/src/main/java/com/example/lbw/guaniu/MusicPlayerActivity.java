9ce5ce745a2d48574b6043c46f9f9d2169c91ed3
liyizhang <yizhang_li@163.com>
2017-08-16 14:42:12 +0800

    add music playey animation

--- app/src/main/java/com/example/lbw/guaniu/MusicPlayerActivity.java
+++ app/src/main/java/com/example/lbw/guaniu/MusicPlayerActivity.java
@@ -11,12 +11,16 @@ import android.widget.LinearLayout;
 import com.example.lbw.guaniu.Music.MusicDiscussDetailActivity;
 import com.example.lbw.guaniu.Square.SquareActivity;
 
+import co.mobiwise.playerview.MusicPlayerView;
+
 /**
  * Created by lbw on 2017/8/9.
  */
 
 public class MusicPlayerActivity extends AppCompatActivity{
     private LinearLayout discuss;
+    MusicPlayerView mpv;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +39,24 @@ public class MusicPlayerActivity extends AppCompatActivity{
             public void onClick(View v) {
                 Intent intent = new Intent(MusicPlayerActivity.this, MusicDiscussDetailActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        initPlayerView();
+    }
+
+    void initPlayerView(){
+
+        mpv = (MusicPlayerView) findViewById(R.id.mpv);
+        mpv.setCoverURL("http://7xiava.com1.z0.glb.clouddn.com/FhtE_xLhl1vC7xACByQVnxduZNmT?imageView2/1/w/320/h/240");
+
+        mpv.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                if (mpv.isRotating()) {
+                    mpv.stop();
+                } else {
+                    mpv.start();
+                }
             }
         });
     }
